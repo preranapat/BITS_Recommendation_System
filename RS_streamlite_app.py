125219faa7a5cb6e73e772247f68af81623c4862
preranapat <136431517+preranapat@users.noreply.github.com>
2023-09-16 12:56:01 +0530

    Update RS_streamlite_app.py

--- RS_streamlite_app.py
+++ RS_streamlite_app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1uyUCOJxutgWL2RiXie1RjDiNozQurSQg
 """
 
-from pickle5 import pickle
+from pickle7 import pickle
 import streamlit as st
 import requests
 import pandas as pd
@@ -27,7 +27,7 @@ def recommend(movie):
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
     recommended_movie_posters = []
-    for i in distances[1:6]:
+    for i in distances[1:8]:
         # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movie_posters.append(fetch_poster(movie_id))
